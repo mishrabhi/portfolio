b0b810b3d0a77b54cdc7336278bfeff7c0b465d8
mishrabhi <asmyselfabhishek00@gmail.com>
2023-08-24 18:48:53 +0530

    added admin dashboard page

--- mypofo/app.js
+++ mypofo/app.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const hbs = require('hbs');
-const middleware = require('./middlewares/middleware');
-const routes = require('./routes');
+const express = require("express");
+const hbs = require("hbs");
+const middleware = require("./middlewares/middleware");
+const routes = require("./routes");
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname+'/views');
-hbs.registerPartials(__dirname+'/views/partials');
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+hbs.registerPartials(__dirname + "/views/partials");
 app.use(middleware.logger);
-app.use(express.static(__dirname+'/static'));
+app.use(express.static(__dirname + "/static"));
 
-
-app.get('/', routes.index);
-app.get('/projects', routes.projectList);
-app.get('/blogs', routes.blogs);
-app.get('/contact', routes.contact);
-app.get('/signin', routes.signIn);
-app.get('/signup', routes.signUp);
-app.get('/projects/:alias', routes.projectDetail);
-app.get('/blogs/:alias', routes.blogDetail);
-
+app.get("/", routes.index);
+app.get("/projects", routes.projectList);
+app.get("/blogs", routes.blogs);
+app.get("/contact", routes.contact);
+app.get("/signin", routes.signIn);
+app.get("/signup", routes.signUp);
+app.get("/admin", routes.admin);
+app.get("/projects/:alias", routes.projectDetail);
+app.get("/blogs/:alias", routes.blogDetail);
 
 app.use(middleware.notFound);
 app.use(middleware.handleError);
 
-
-app.listen(3000, () => console.log('App up and running on port 3000'));
+app.listen(3000, () => console.log("App up and running on port 3000"));
--- mypofo/routes/index.js
+++ mypofo/routes/index.js
@@ -1,59 +1,64 @@
-exports.index = (req,res) => {
-    res.render('index', {
-        title: 'Abhishek - Portfolio',
-        layout: 'layout'
-    });
-}
+exports.index = (req, res) => {
+  res.render("home", {
+    title: "Abhishek - Portfolio",
+    layout: "homeLayout",
+  });
+};
 
 exports.projectList = (req, res) => {
-    res.render('projects', {
-        title: 'Projects',
-        navProject: true,
-        layout: 'layout'
-    });
-}
+  res.render("projects", {
+    title: "Projects",
+    navProject: true,
+    layout: "layout",
+  });
+};
 
-exports.blogs = (req,res) => {
-    res.render('blogs', {
-        title: 'Blogs',
-        layout: 'layout',
-        navBlogs: true
-    });
-}
+exports.blogs = (req, res) => {
+  res.render("blogs", {
+    title: "Blogs",
+    layout: "layout",
+    navBlogs: true,
+  });
+};
 
-exports.contact = (req,res) => {
-    res.render('contact', {
-        title: 'contact',
-        layout: 'layout',
-        mavContact: true
-    });
-}
+exports.contact = (req, res) => {
+  res.render("contact", {
+    title: "contact",
+    layout: "layout",
+    mavContact: true,
+  });
+};
 
 exports.projectDetail = (req, res) => {
-    res.render('projectDetail', {
-        title: 'Project Detail',
-        layout: 'layout'
-    });
-}
+  res.render("projectDetail", {
+    title: "Project Detail",
+    layout: "layout",
+  });
+};
 
 exports.blogDetail = (req, res) => {
-    res.render('blogDetail', {
-        title: 'blogDetail',
-        layout: 'layout'
-    });
-}
-
+  res.render("blogDetail", {
+    title: "blogDetail",
+    layout: "layout",
+  });
+};
 
 exports.signIn = (req, res) => {
-    res.render('signin', {
-        title: 'signin',
-        layout: 'loginLayout'
-    });
-}
+  res.render("signin", {
+    title: "signin",
+    layout: "loginLayout",
+  });
+};
 
 exports.signUp = (req, res) => {
-    res.render('signup', {
-        title: 'Signup',
-        layout: 'loginlayout'
-    });
-}
+  res.render("signup", {
+    title: "Signup",
+    layout: "loginlayout",
+  });
+};
+
+exports.admin = (req, res) => {
+  res.render("admin/index", {
+    layout: "adminLayout",
+  });
+};
